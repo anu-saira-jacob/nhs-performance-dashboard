33891ba5990a297eecbf6ba989eeadb7a453b65d
anu-saira-jacob <anusairajacob@gmail.com>
2025-08-21 18:42:06 +0100

    remove db username and password from git

--- src/db.py
+++ src/db.py
@@ -3,11 +3,11 @@ from sqlalchemy import create_engine
 import pandas as pd
 
 # To keep DB credentials
-DB_USER = "postgres"
-DB_PASS = "postgres"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "nhs_dashboard"
+DB_USER = "<your_db_user>"
+DB_PASS = "<your_db_password>"
+DB_HOST = "<your_db_host>"
+DB_PORT = "<your_db_port>"
+DB_NAME = "<your_db_name>"
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
